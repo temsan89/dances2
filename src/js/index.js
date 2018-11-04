29797dacee2e7c2d72bfb72b738268108a6502c5
Tetiana Hrytsaienko <kortatiana89@gmail.com>
2018-11-04 12:52:51 +0200

    Collage added, info About us

--- src/js/index.js
+++ src/js/index.js
@@ -40,4 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems);
+
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
 });
